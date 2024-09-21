4a0e05b2f9e263185b32ab1c5c6c80400ce77849
Ramón García Espejo <ramon.garcia5800@alumnos.udg.mx>
2024-09-21 11:21:25 -0600

    Add a enemy and combat system

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -4,7 +4,13 @@ var speed = 60
 var player_chase = false
 var player = null
 
+var health = 100
+var player_inattack_zone = false
+var can_take_damage = true
+
 func _physics_process(delta):
+	deal_with_damage()
+	
 	if player_chase:
 		position += (player.position - position)/ speed
 		$AnimatedSprite2D.play("side_walk")
@@ -15,12 +21,42 @@ func _physics_process(delta):
 	else:
 		$AnimatedSprite2D.play("front_idle")
 
+
+
 func _on_detection_area_body_entered(body):
 	player = body
 	player_chase = true
 	
 
-
 func _on_detection_area_body_exited(body: Node2D) -> void:
 	player = null
 	player_chase = false
+
+func enemy():
+	pass
+	
+
+
+func _on_enemy_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone = true
+
+
+func _on_enemy_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone = false
+		
+func deal_with_damage():
+	if player_inattack_zone and global.player_current_attack == true:
+		if can_take_damage == true:
+			health = health - 20
+			$take_damage_cooldown.start()
+			can_take_damage = false
+			print("Slime health = ", health)
+			if health <= 0:
+				self.queue_free()
+	
+
+
+func _on_take_damage_cooldown_timeout() -> void:
+	can_take_damage = true
